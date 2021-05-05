240aea513e24ab99f0cb20215af9e9768d766942
vastutsav <utsavbarman@gmail.com>
2021-05-06 00:58:01 +0530

    Corrected filename

--- SameDifferences1520D.java
+++ SameDifferences1520D.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class SameDifferences1520C {
+public class SameDifferences1520D {
  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
