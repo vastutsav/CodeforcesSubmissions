813fa16ac907d18e773d8430c25ff3374f607fec
vastutsav <utsavbarman@gmail.com>
2021-05-06 01:28:45 +0530

    Fixing TLE

--- SameDifferences1520D.java
+++ SameDifferences1520D.java
@@ -1,25 +1,33 @@
+import java.util.HashMap;
 import java.util.Scanner;
+
  
 public class SameDifferences1520D {
  
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		HashMap <Integer, Integer> hmap = new HashMap <Integer, Integer> ();
 		int T = sc.nextInt();
 		for (int t = 0; t < T; ++t) {
 			int n = sc.nextInt();
-			int [] arr = new int [200001];
-			for (int i = 0; i < n; ++i) {
-				arr[i] = sc.nextInt();
-			}
 			int cnt = 0;
-			for (int i = 0; i < n; ++i ) {
-				for (int j = i+1; j < n; ++j) {
-					if ((i-j) == (arr[i] - arr[j])) {
-						cnt++;
-					}
+			int arr = 0;
+			for (int i = 0; i < n; ++i) {
+				arr = sc.nextInt() - i;
+				int a = 1;
+			 	if (hmap.containsKey(arr)) {
+					a = hmap.get(arr) + 1;
 				}
+				
+				hmap.put(arr, a);
 			}
+			
+			for (Integer k : hmap.keySet()) {
+				int v = hmap.get(k);
+				cnt += (int) ((v)*(v-1)/2);
+			}
+
+			hmap.clear();
 			System.out.println(cnt);
 		}
 		sc.close();
